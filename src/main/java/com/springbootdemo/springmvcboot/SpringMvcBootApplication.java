db4949bddd2c662cc6995976ae9407343f12f463
ssandyy <ssandyy789@gmail.com>
2024-02-23 18:49:34 +0530

    first commit

---
+++ src/main/java/com/springbootdemo/springmvcboot/SpringMvcBootApplication.java
@@ -0,0 +1,13 @@
+package com.springbootdemo.springmvcboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMvcBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springbootdemo/springmvcboot/SpringMvcBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootdemo.springmvcboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
